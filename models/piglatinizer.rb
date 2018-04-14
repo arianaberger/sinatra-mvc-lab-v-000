bf118af25c143333af367fb359a703d035fcfdd8
Ariana Berger <ariana.berger@gmail.com>
2018-04-14 17:52:17 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,8 +8,8 @@ class PigLatinizer
   #
   def piglatinize(word)
     vowels = %w{a e i o u}
-    word.gsub(/(\A|\s)\w+/) do |string|
-      string.strip!
+    word.gsub(/(\A|\s)\w+/) do |str|
+      str.strip!
       while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
         str += str[0]
         str = str[1..-1]
