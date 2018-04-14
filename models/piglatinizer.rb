1e46a0e74e1b5df5078e203c9ab5ae083019e4c6
Ariana Berger <ariana.berger@gmail.com>
2018-04-14 18:01:11 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,17 +6,17 @@ class PigLatinizer
   #   # @text = text.downcase
   # end
   #
-  def piglatinize(word)
-    vowels = %w{a e i o u}
-    word.gsub(/(\A|\s)\w+/) do |str|
-      str.strip!
-      while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
-        str += str[0]
-        str = str[1..-1]
-      end
-      str  = ' ' + str + 'ay'
-    end.strip
-  end
+  # def piglatinize(word)
+  #   vowels = %w{a e i o u}
+  #   word.gsub(/(\A|\s)\w+/) do |str|
+  #     str.strip!
+  #     while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
+  #       str += str[0]
+  #       str = str[1..-1]
+  #     end
+  #     str  = ' ' + str + 'ay'
+  #   end.strip
+  # end
 
 
 end
